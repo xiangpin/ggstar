e79caa54b53d4393cc0e52dcd517029bbc4ee9cc
xiangpin <xshuangbin@163.com>
2020-03-13 12:19:49 +0800

    viewport optimize

--- R/primitive.R
+++ R/primitive.R
@@ -1,46 +1,130 @@
-#' @importFrom grid unit convertX convertY is.unit 
-#' @importFrom gridExtra polygon_regular
+#' @importFrom grid unit convertX convertY is.unit unit.c
 starGrob <- function(x=0.5, y=0.5,
                      starshape=1, size=2, 
-                     angle=0, ar=1,
+                     angle=0, 
                      phase = 0,
-                     gp = gpar(col=NA, 
-                               fill="black"),
+                     gp = gpar(fill="black",
+                               alpha=1,
+                               col=NA,
+                               lwd=0.5),
                      position.units = "npc", 
                      size.units="mm", ...){
-    stopifnot(length(x)==1)
-    stopifnot(length(x)==length(y))
+    if (! all(starshape %in% seq_len(12))){
+        stop("the starshape should be one of 1 to 12 !")
+    }
+    N <- length(x)
+    stopifnot(length(y)==N)
     if (!is.unit(x)){x <- unit(x, position.units)}
     if (!is.unit(y)){y <- unit(y, position.units)}
-    
     xv <- convertX(x, position.units, TRUE)
     yv <- convertY(y, position.units, TRUE)
-    
-    stargrob <- .starGrob(xv=xv, yv=yv,
-                           starshape=starshape,
-                           size = size,
-                           angle = angle, 
-                           ar = ar,
-                           phase = phase,
-                           gp = gp,
-                           position.units=position.units,
-                           size.units=size.units, ...)
-    return (stargrob)
+    n <- match_n(starshape)
+    plxy_ids <- mapply(build_polygenxy_id.lengths, 
+                       starshape=starshape, 
+                       phase=phase, SIMPLIFY=FALSE)
+    lnxy <- lapply(plxy_ids, function(x)x$plxy)
+    vertices <- unlist(lapply(plxy_ids, function(x)x$ids))
+    # ar is the aspect ratio. It can control the 
+    # height and width ratio of shapes.
+    ar <- match_ar(starshape)
+    lxy <- mapply(stretch_rotate_move, p=lnxy,
+                  size=size, ar=ar, angle=angle, 
+                  x=xv, y=yv, 
+                  MoreArgs=list(position.units=position.units,
+                                size.units=size.units),
+                  SIMPLIFY = FALSE)
+    allx <- do.call("unit.c", lapply(lxy, "[[", 1))
+    ally <- do.call("unit.c", lapply(lxy, "[[", 2))
+    grobs <- polygonGrob(allx, ally, id.lengths = vertices, gp = gp, ...) 
+    return(grobs)
+}
+
+# index of starshape = numbers of edge (n)
+starshape_ntab <- c(5, 3, 7, 4,
+                    5, 6, 7, 8,
+		    8, 8, 3, 4)
+
+names(starshape_ntab) <- seq_len(12)
+
+match_n <- function(starshape){
+    n <- starshape_ntab[match(starshape,names(starshape_ntab))]
+    return(unname(n))
+}
+
+# index of starshape = aspect ratio (ar) 
+starshape_artab <- c(rep(1, 9),0.5, 1, 0.5)
+names(starshape_artab) <- seq_len(12)
+
+match_ar <- function(starshape){
+    ar <- starshape_artab[match(starshape,names(starshape_artab))]
+    return(unname(ar))
+}
+
+build_starshape_attr <- function(starshape, fill, col, lwd, alpha){
+    if (starshape==2 || starshape==4){
+        fill <- rep(fill, 2)
+        col <- rep(col, 2)
+        lwd <- rep(lwd, 2)
+        alpha <- rep(alpha, 2)
+    }
+    allattr <- list(fill=fill, col=col, lwd=lwd, alpha=alpha)
+    return(allattr)
+}
+
+mutate_attr <- function(mapplyres, attrstr){
+    res <- unlist(lapply(mapplyres, function(x)x[[attrstr]]))
+    return(res)
+}
+
+#' @importFrom gridExtra polygon_regular
+build_polygenxy_id.lengths <- function(starshape, phase){
+    # the edge numbers
+    n <- match_n(starshape)
+    if (starshape==1){
+        plxy <- polygon_regular(n=n, phase=phase)
+        tmpx <- plxy[,1][c(1, 3, 5, 2, 4)]
+        tmpy <- plxy[,2][c(1, 3, 5, 2, 4)]
+        plxy <- cbind(tmpx, tmpy, deparse.level = 0)
+        ids <- nrow(plxy)
+    }else if(starshape==3){
+        plxy <- polygon_regular(n=n, phase=phase)
+        tmpx <- plxy[,1][c(1, 4, 7, 3, 6, 2, 5)]
+        tmpy <- plxy[,2][c(1, 4, 7, 3, 6, 2, 5)]
+        plxy <- cbind(tmpx, tmpy, deparse.level = 0)
+        ids <- nrow(plxy)
+    }else if (starshape==2 || starshape==4){
+        phase2 <- phase + pi/n
+        plxy <- mapply(polygon_regular, 
+               phase=c(phase, phase2), 
+               n=rep(n, 2), SIMPLIFY=FALSE)
+        plxy <- do.call("rbind", plxy)
+        ids <- rep(nrow(plxy)/2, 2)
+    }else if(starshape==9 || starshape==10){
+        plxy <- polygon_regular(n=n, phase=phase)
+        tmpx <- plxy[,1][c(1, 6, 3, 8, 5, 2, 7, 4)]
+        tmpy <- plxy[,2][c(1, 6, 3, 8, 5, 2, 7, 4)]
+        plxy <- cbind(tmpx, tmpy, deparse.level = 0)
+        ids <- nrow(plxy)
+    }
+    else{
+        plxy <- polygon_regular(n=n, phase=phase)
+        ids <- nrow(plxy)
+    }
+    return(list(plxy=plxy, ids=ids))
 }
 
 #' @importFrom grid grid.draw
 grid.star <- function(x=0.5, y=0.5,
                       starshape=1, size=2,
-                      angle=0, ar=1,
+                      angle=0, 
                       phase = 0,
                       gp = NULL,
                       position.units = "npc",
                       size.units="mm",
                       draw = TRUE, vp = NULL, ...){
     sg <- starGrob(x = x, y = y, 
-                   starshape = 1, size = 2, 
+                   starshape = starshape, size = size, 
                    angle = angle, 
-                   ar = ar,
                    gp = gp, 
                    position.units = position.units,
                    size.units = size.units,
@@ -50,98 +134,6 @@ grid.star <- function(x=0.5, y=0.5,
     }
     invisible(sg)
 }
-
-#' @importFrom gridExtra polygon_regular
-#' @importFrom grid gList polygonGrob
-.starGrob <- function(xv, yv, 
-                       starshape, size,
-                       angle, ar,
-                       phase,
-                       gp,
-                       position.units,
-                       size.units, ...){
-    if (!starshape%in%seq_len(12)){
-        stop("the starshape should be one of 1 to 12 !")
-    }
-    if (starshape==1){
-        locoord <- polygon_regular(n=5, phase=phase)
-    }
-    else if (starshape==2){
-        locoord <- polygon_regular(n=6, phase=phase)
-    }
-    else if (starshape==3){
-        locoord <- polygon_regular(n=7, phase=phase)
-    }
-    else if (starshape==4 || starshape==8 || starshape==9 || starshape==10){
-        locoord <- polygon_regular(n=8, phase=phase)
-    }
-    else if (starshape==5){
-        locoord <- polygon_regular(n=5, phase=phase)
-    }
-    else if (starshape==6){
-        locoord <- polygon_regular(n=6, phase=phase)
-    }
-    else if (starshape==7){
-        locoord <- polygon_regular(n=7, phase=phase)
-    }
-    else if (starshape==11){
-        locoord <- polygon_regular(n=3, phase=phase)
-    }
-    else if (starshape==12){
-        locoord <- polygon_regular(n=4, phase=phase)
-        ar <- 0.5
-    }
-    if (starshape == 10) {ar <- 0.5}
-    lxy <- stretch_rotate_move(p=locoord,
-                               size = size,
-                               ar = ar,
-                               angle = angle,
-                               x = xv, y = yv,
-                               position.units = position.units,
-                               size.units=size.units)
-    if (starshape==1){
-        x <- lxy$x[c(1, 3, 5, 2, 4)]
-        y <- lxy$y[c(1, 3, 5, 2, 4)]
-    }
-    else if (starshape==2 || starshape==4){
-        x1 <- oddelement(lxy$x)
-        y1 <- oddelement(lxy$y) 
-        x2 <- evenelement(lxy$x)
-        y2 <- evenelement(lxy$y)
-    }
-    else if (starshape==3){
-        x <- lxy$x[c(1, 4, 7, 3, 6, 2, 5)]
-        y <- lxy$y[c(1, 4, 7, 3, 6, 2, 5)]
-    }
-    else if (starshape==9 || starshape==10){
-        x <- lxy$x[c(1, 6, 3, 8, 5, 2, 7, 4)]
-        y <- lxy$y[c(1, 6, 3, 8, 5, 2, 7, 4)]
-    }
-    else{
-        x <- lxy$x
-        y <- lxy$y
-    }
-    if (starshape==2 || starshape==4){
-        grobres <- gTree(children=gList(polygonGrob(x1, y1, gp = gp),
-                         polygonGrob(x2, y2, gp = gp)), ...)
-    }
-    else{
-        grobres <- polygonGrob(x, y, gp = gp, ...)
-    }
-    return(grobres)
-}
-
-evenelement <- function(x){
-    elelist <- seq_len(length(x))-1
-    x[is.even(elelist)]
-}
-
-oddelement <- function(x){
-    elelist <- seq_len(length(x))-1
-    x[!is.even(elelist)]
-}
-
-is.even <- function(x) x%%2==0
 
 # reference the gridExtra
 stretch_rotate_move <- function(p, size, 
@@ -154,3 +146,4 @@ stretch_rotate_move <- function(p, size,
     list(x = unit(central[,1], size.units) + unit(x, position.units),
 	 y = unit(central[,2], size.units) + unit(y, position.units))
 }
+
