317065a65a3de9eee264339d0ea6aa68d6032dd4
xiangpin <xshuangbin@163.com>
2020-05-22 11:26:16 +0800

    add scale_starshape_identity function

---
+++ R/scale-identity.R
@@ -0,0 +1,12 @@
+#' Use values without scaling
+#' 
+#' @inheritDotParams ggplot2::continuous_scale
+#' @param guide Guide to use for this scale. Defaults to `"none"`.
+#' @seealso \code{\link[ggplot2]{scale_shape_identity}}
+#' @importFrom ggplot2 continuous_scale ScaleContinuousIdentity
+#' @export
+scale_starshape_identity <- function(..., guide = "none") {
+    sc <- continuous_scale("starshape", "identity", scales::identity_pal(), ..., guide = guide,
+                           super = ScaleContinuousIdentity)
+    sc
+}
