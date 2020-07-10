e219bc4572f8f6cf73ba3df24c5eede29e96a03d
xiangpin <xshuangbin@163.com>
2020-07-10 19:49:25 +0800

    update rd

--- R/scale-identity.R
+++ R/scale-identity.R
@@ -1,4 +1,4 @@
-#' Use values without scaling
+#' Use values without scaling for ggstar
 #' 
 #' @inheritDotParams ggplot2::continuous_scale
 #' @param guide Guide to use for this scale. Defaults to `"none"`.
