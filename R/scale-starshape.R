0e12bfc07379119fbc102a3980a908080402e4de
xiangpin <xshuangbin@163.com>
2021-12-03 10:35:12 +0800

    change the default starshape

--- R/scale-starshape.R
+++ R/scale-starshape.R
@@ -46,7 +46,7 @@ starshape_pal <- function(default=TRUE){
         warning(paste(strwrap(msg), collapse = "\n"), call. = FALSE)
     }
     if (default){
-        c(1, 2, 9, 4, 12, 14, 29, 6, 27)[seq_len(n)]
+        c(1, 13, 15, 11, 12, 14, 29, 2, 27)[seq_len(n)]
     }else{
         c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)[seq_len(n)]
     }
